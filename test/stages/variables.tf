e72c5f7126c2633bb1d91633c370862e71b910bf
Andrew Trice <amtrice@us.ibm.com>
2022-07-13 14:58:08 -0400

    switched back to portworx storageclass
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -96,7 +96,7 @@ variable "kubeseal_namespace" {
 variable "storageClass" {
   type        = string
   description = "The name of the server"
-  default     = "ocs-storagecluster-cephfs"
+  default     = "portworx-db2-rwx-sc"
 }
 
 variable "database_name" {
