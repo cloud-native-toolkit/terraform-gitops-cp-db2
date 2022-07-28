9da78a59b8c507015b679eddb11554ed8138490b
Andrew Trice <amtrice@us.ibm.com>
2022-07-28 17:07:15 -0400

    updated db2wh kernel setup script
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- .github/scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-echo "DB2 Operator uninstall"
-
-#oc get Db2whService -n project-name
-
-#oc delete csv ${CSV} -n ${OPERATOR_NAMESPACE}
-
-#oc delete Db2whService db2wh-cr -n ${CPD_NAMESPACE}
-#
-#oc delete csv ${CSV} -n ${OPERATOR_NAMESPACE}
